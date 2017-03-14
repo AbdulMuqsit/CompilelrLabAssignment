030c8d0314c98332789da79a243482b6dac48e8d
AMuqsit <abdemuqsit@hotmail.com>
2017-03-14 19:33:34 +0500

    Ifrastructre done

--- CompilelrLabAssignment/Infrastructure/ViewModelLocator.cs
+++ CompilelrLabAssignment/Infrastructure/ViewModelLocator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CompilelrLabAssignment.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace CompilelrLabAssignment.Infrastructure
 {
     public class ViewModelLocator
     {
+        public ViewModelLocator()
+        {
+            MainViewModel = new MainViewModel();
+        }
+        public MainViewModel MainViewModel { get; }
     }
 }
