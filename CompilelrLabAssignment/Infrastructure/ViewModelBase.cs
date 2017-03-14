6322ea2194fc3b74b9c54b0d5c3d76bf781d4014
AMuqsit <abdemuqsit@hotmail.com>
2017-03-14 19:12:41 +0500

    Add project files.

---
+++ CompilelrLabAssignment/Infrastructure/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompilelrLabAssignment.Infrastructure
+{
+    public class ViewModelBase
+    {
+        private ViewModelLocator _viewModelLocator;
+
+        protected ViewModelLocator ViewModelLocator
+        {
+            get { return _viewModelLocator ?? (_viewModelLocator = (ViewModelLocator)App.Current.Resources["ViewModelLocator"]); }
+        }
+
+    }
+}
